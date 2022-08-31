e1f0d8e4b53aec36a7568a7869a8e6f4ad8d6c5a
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-08-31 08:50:58 +0100

    Docstrings reviewed, implemented training and evaluation per feature

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,54 +6,46 @@ import keras
 import pandas as pd
 from dvc.api import params_show
 from joblib import load
-from dvclive.keras import DvcLiveCallback
-from features import FORECAST_FEATURES
+
 from features.create_sequences import create_sequences
-
-
-def read_model(model_path, model):
-    if 'scaler' in model:
-        return load(os.path.join(model_path, model))
-    return keras.models.load_model(os.path.join(model_path, model))
 
 
 @click.command()
 @click.argument('local_test_data_path', type=click.Path(exists=True))
 @click.argument('local_models_path', type=click.Path(exists=True))
 @click.argument('local_metrics_path', type=click.Path(exists=False))
-def evaluate(local_test_data_path, local_models_path, local_metrics_path):
-    """
-    Evaluate the model.
+@click.argument('feature', type=click.STRING)
+def evaluate(local_test_data_path, local_models_path, local_metrics_path,
+             feature):
+    """Takes the model from the local model directory, evaluates it on the test data set, saves the resultant metrics to the local metrics directory
 
-    Returns
-    -------
-    None
-    
+    Args:
+        local_test_data_path (str): Test data directory
+        local_models_path (str): Models directory
+        local_metrics_path (str): Metrics directory
+        feature (str): Feature
     """
     params = params_show()
     test_df = pd.read_csv(os.path.join(local_test_data_path, 'test_data.csv'),
                           index_col=False)
     if not os.path.exists(local_metrics_path):
         os.makedirs(local_metrics_path)
-    for feature in FORECAST_FEATURES:
-        model = f'RNN_{feature}'
-        test_data = test_df[feature].values.reshape(-1, 1)
-        scaler = read_model(local_models_path, model + '_scaler.joblib')
-        scaled_test = scaler.transform(test_data)
-        x_test, y_test = create_sequences(
-            scaled_test,
-            lookback=params['model']['look_back'],
-            inference=False)
-        forecaster = read_model(local_models_path, model + '.h5')
-        results = forecaster.evaluate(x_test,
-                                      y_test,
-                                      verbose=1,
-                                      batch_size=params['train']['batch_size'],
-                                      return_dict=True)
-        with open(os.path.join(local_metrics_path, f'eval_{model}.json'),
-                  'w') as f:
-            f.write(json.dumps(results))
-        if params['one_feature']: break
+    test_data = test_df[feature].values.reshape(-1, 1)
+    scaler = load(os.path.join(local_models_path, feature + '.joblib'))
+    scaled_test = scaler.transform(test_data)
+    x_test, y_test = create_sequences(scaled_test,
+                                      lookback=params['model']['look_back'],
+                                      inference=False)
+    forecaster = keras.models.load_model(
+        os.path.join(local_models_path, feature + '.h5'))
+    results = forecaster.evaluate(x_test,
+                                  y_test,
+                                  verbose=1,
+                                  batch_size=params['train']['batch_size'],
+                                  return_dict=True)
+    with open(os.path.join(local_models_path, f'eval_{feature}.json'),
+              'w') as f:
+        f.write(json.dumps(results))
 
 
 if __name__ == '__main__':
--- src/train.py
+++ src/train.py
@@ -8,7 +8,6 @@ from dvc.api import params_show
 from joblib import dump
 from sklearn.preprocessing import MinMaxScaler
 
-from features import FORECAST_FEATURES
 from features.create_sequences import create_sequences
 
 
@@ -16,13 +15,16 @@ from features.create_sequences import create_sequences
 @click.argument('local_train_data_path', type=click.Path(exists=True))
 @click.argument('local_models_path', type=click.Path(exists=False))
 @click.argument('local_metrics_path', type=click.Path(exists=False))
-def train(local_train_data_path, local_models_path, local_metrics_path):
-    """
-    Train the model.
+@click.argument('feature', type=click.STRING)
+def train(local_train_data_path, local_models_path, local_metrics_path,
+          feature):
+    """Defines and trains the model, saves the model and its metrics to the specified paths.
 
-    Returns
-    -------
-    None
+    Args:
+        local_train_data_path (str): Train data directory
+        local_models_path (str): Models directory
+        local_metrics_path (str): Metrics directory
+        feature (str): Feature to train for
     """
     params = params_show()
     train_df = pd.read_csv(os.path.join(local_train_data_path,
@@ -30,54 +32,50 @@ def train(local_train_data_path, local_models_path, local_metrics_path):
                            index_col=False)
     if not os.path.exists(local_models_path):
         os.makedirs(local_models_path)
-    for feature in FORECAST_FEATURES:
-        model = f'RNN_{feature}'
-        train_data = train_df[feature].values.reshape(-1, 1)
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled_train = scaler.fit_transform(train_data)
-        dump(scaler, os.path.join(local_models_path, f'{model}_scaler.joblib'))
-        x_train, y_train = create_sequences(
-            scaled_train,
-            lookback=params['model']['look_back'],
-            inference=False)
-        forecaster = keras.models.Sequential()
-        forecaster.add(
-            keras.layers.LSTM(params['model']['lstm_units'],
-                              input_shape=(x_train.shape[1], x_train.shape[2]),
-                              return_sequences=False))
-        forecaster.add(keras.layers.Dense(1))
-        forecaster.compile(loss=keras.losses.mean_squared_error,
-                           metrics=keras.metrics.RootMeanSquaredError(),
-                           optimizer=keras.optimizers.RMSprop(
-                               learning_rate=params['train']['learning_rate']))
-        history = forecaster.fit(
-            x_train,
-            y_train,
-            shuffle=False,
-            epochs=params['train']['epochs'],
-            batch_size=params['train']['batch_size'],
-            validation_split=0.2,
-            verbose=1,
-            callbacks=[
-                keras.callbacks.EarlyStopping(
-                    patience=params['train']['patience'],
-                    monitor='val_loss',
-                    mode='min',
-                    verbose=1,
-                    restore_best_weights=True),
-                keras.callbacks.ReduceLROnPlateau(
-                    monitor='val_loss',
-                    factor=0.75,
-                    patience=params['train']['patience'] // 2,
-                    verbose=1,
-                    mode='min')
-            ])
-        with open(os.path.join(local_metrics_path, f'train_{model}.json'), 'w') as f:
-            for k, v in history.history.items():
-                history.history[k] = [float(vi) for vi in v]
-            f.write(json.dumps(history.history))
-        forecaster.save(os.path.join(local_models_path, f'{model}.h5'))
-        if params['one_feature']: break
+    train_data = train_df[feature].values.reshape(-1, 1)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_train = scaler.fit_transform(train_data)
+    dump(scaler, os.path.join(local_models_path, f'{feature}.joblib'))
+    x_train, y_train = create_sequences(scaled_train,
+                                        lookback=params['model']['look_back'],
+                                        inference=False)
+    forecaster = keras.models.Sequential()
+    forecaster.add(
+        keras.layers.LSTM(params['model']['lstm_units'],
+                          input_shape=(x_train.shape[1], x_train.shape[2]),
+                          return_sequences=False))
+    forecaster.add(keras.layers.Dense(1))
+    forecaster.compile(loss=keras.losses.mean_squared_error,
+                       metrics=keras.metrics.RootMeanSquaredError(),
+                       optimizer=keras.optimizers.RMSprop(
+                           learning_rate=params['train']['learning_rate']))
+    history = forecaster.fit(x_train,
+                             y_train,
+                             shuffle=False,
+                             epochs=params['train']['epochs'],
+                             batch_size=params['train']['batch_size'],
+                             validation_split=0.2,
+                             verbose=1,
+                             callbacks=[
+                                 keras.callbacks.EarlyStopping(
+                                     patience=params['train']['patience'],
+                                     monitor='val_loss',
+                                     mode='min',
+                                     verbose=1,
+                                     restore_best_weights=True),
+                                 keras.callbacks.ReduceLROnPlateau(
+                                     monitor='val_loss',
+                                     factor=0.75,
+                                     patience=params['train']['patience'] // 2,
+                                     verbose=1,
+                                     mode='min')
+                             ])
+    with open(os.path.join(local_metrics_path, f'train_{feature}.json'),
+              'w') as f:
+        for k, v in history.history.items():
+            history.history[k] = [float(vi) for vi in v]
+        f.write(json.dumps(history.history))
+    forecaster.save(os.path.join(local_models_path, f'{feature}.h5'))
 
 
 if __name__ == '__main__':
